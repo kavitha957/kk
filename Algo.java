593b55d79deb0efac2a773f068b2ed8e3d4da878
kavitha957 <kavithasna14@gmail.com>
2016-08-23 20:43:39 +0530

    Add files via upload

---
+++ Algo.java
@@ -0,0 +1,7 @@
+
+
+) Create a temp array temp[] of size n with all initial values as 0.
+2) Traverse the input array arr[], and do following for each arr[i]
+……a) if(temp[arr[i]] == 0) temp[arr[i]] = 1;
+……b) if(temp[arr[i]] == 1) output “arr[i]” //repeating
+3) Traverse temp[] and output the array element having value as 0 (This is the missing element)
